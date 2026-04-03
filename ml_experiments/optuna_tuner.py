26c161716d1daa0ea91eb6f948ff06603aedefc9
Emmanuel Maximus <emmanuelmaximus2470033@ssn.edu.in>
2026-04-03 17:31:45 +0530

    Initial prep commit, for overhaul

--- ml_experiments/optuna_tuner.py
+++ ml_experiments/optuna_tuner.py
@@ -16,6 +16,7 @@ Usage:
 
 import os
 import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import argparse
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
